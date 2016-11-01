336b18435f4a6803b42a02bea5a3cab85ec2fef8
Lawrence O'Boyle II <lawrence@oboylelii.com>
2016-11-01 16:55:53 -0400

    Unrolled the addition for parral adding into a block size

--- dotproduct.cu
+++ dotproduct.cu
@@ -59,28 +59,86 @@ extern "C" void gen_curand_array(unsigned long long seed, int * array,
     GPU_ERR_CHK(cudaFree(array_d));
 }
 
-__global__ void cu_dot(int * a_d, int * b_d, int * block_results_d, size_t size) {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    __shared__ int cache[BLOCK_SIZE];
-    if (idx < size) {
-        cache[threadIdx.x] = a_d[idx] * b_d[idx];
-    }else{
-        cache[threadIdx.x] = 0;
-    }
-    __syncthreads();
+template <unsigned int block_size>
+__global__ void cu_dot(int * a_d, int * b_d, int * block_results_d,
+        size_t size) {
+    extern __shared__ int cache[];
 
-    if(threadIdx.x == 0) {
-        block_results_d[blockIdx.x] = 0;
-        for(int z = 0; z < BLOCK_SIZE; z++) {
-            block_results_d[blockIdx.x] += cache[z];
+    unsigned int tid = threadIdx.x;
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    cache[tid] = 0;
+
+    if(idx < size) {
+        cache[tid] = a_d[idx] * b_d[idx];
+    }
+
+    __syncthreads();
+    if(block_size >= 512) {
+        if(tid < 256) {
+            cache[tid] += cache[tid + 256];
         }
+        __syncthreads();
+    }
+
+    if(block_size >= 256) {
+        if(tid < 128) {
+            cache[tid] += cache[tid + 128];
+        }
+        __syncthreads();
+    }
+
+    if(block_size >= 128) {
+        if(tid < 64) {
+            cache[tid] += cache[tid + 64];
+        }
+        __syncthreads();
+    }
+
+    if(tid < 32) {
+        if(block_size >= 64) {
+            cache[tid] += cache[tid + 32];
+        }
+        __syncthreads();
+
+        if(block_size >= 32) {
+            cache[tid] += cache[tid + 16];
+        }
+        __syncthreads();
+
+        if(block_size >= 16) {
+            cache[tid] += cache[tid + 8];
+        }
+        __syncthreads();
+
+        if(block_size >= 8) {
+            cache[tid] += cache[tid + 4];
+        }
+        __syncthreads();
+
+        if(block_size >= 4) {
+            cache[tid] += cache[tid + 2];
+        }
+        __syncthreads();
+
+        if(block_size >= 2) {
+            cache[tid] += cache[tid + 1];
+        }
+    }
+
+    __syncthreads();
+    if(tid == 0) {
+        block_results_d[blockIdx.x] = cache[0];
     }
 }
 
-extern "C" void dot_product(int * result, int * a, int * b, size_t size) {
+extern "C" void dot_product(long long * result, int * a, int * b, size_t size) {
     int * a_d;
     int * b_d;
-    int blocks = ceil(size / ((float) BLOCK_SIZE));
+    int blocks = 1;
+    if(size > BLOCK_SIZE) {
+        blocks = ceil(((float)size) / BLOCK_SIZE);
+    }
+    int cache_size = BLOCK_SIZE * sizeof(int); 
     int * block_results_d;
     int * block_results = (int *) malloc(blocks * sizeof(int));
     dim3 dimgrid (blocks);
@@ -95,8 +153,8 @@ extern "C" void dot_product(int * result, int * a, int * b, size_t size) {
     GPU_ERR_CHK(cudaMemcpy(b_d, b, sizeof(int) * size,
                 cudaMemcpyHostToDevice));
 
-    cu_dot <<<dimgrid, dimblock>>> (a_d, b_d, block_results_d, size);
-
+    //cu_dot<BLOCK_SIZE> <<<dimgrid, dimblock, cache_size>>>(a_d, b_d, block_results_d, size);
+    cu_dot<BLOCK_SIZE> <<<blocks, BLOCK_SIZE, cache_size>>>(a_d, b_d, block_results_d, size);
 
     GPU_ERR_CHK(cudaMemcpy(block_results, block_results_d,
                 blocks * sizeof(int), cudaMemcpyDeviceToHost));
@@ -104,6 +162,8 @@ extern "C" void dot_product(int * result, int * a, int * b, size_t size) {
     for(int z = 0; z < blocks; z++) {
         *result += block_results[z];
     }
+
+    //*result = *block_results;
 
     cudaFree(a_d);
     cudaFree(b_d);
